869889ad3e61f95533a51905fb71262d396a6a85
Juan Carlos Cardenas Viera <jcc@Juans-MacBook-Pro.local>
2017-11-05 18:18:48 -0600

    bootstrap sb admin template

---
+++ static/dist/js/sb-admin-2.js
@@ -0,0 +1,59 @@
+/*!
+ * Start Bootstrap - SB Admin 2 v3.3.7+1 (http://startbootstrap.com/template-overviews/sb-admin-2)
+ * Copyright 2013-2016 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
+$(function() {
+    $('#side-menu').metisMenu();
+});
+
+var slider = new Slider("#ex22", {
+    id: 'slider22',
+    min: 0,
+    max: 20,
+    step: 1,
+    value: 14,
+    rangeHighlights: [{ "start": 2, "end": 5, "class": "category1" },
+                      { "start": 7, "end": 8, "class": "category2" },
+                      { "start": 17, "end": 19 },
+                      { "start": 17, "end": 24 },
+                      { "start": -3, "end": 19 }]});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function() {
+    $(window).bind("load resize", function() {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    // var element = $('ul.nav a').filter(function() {
+    //     return this.href == url;
+    // }).addClass('active').parent().parent().addClass('in').parent();
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).addClass('active').parent();
+
+    while (true) {
+        if (element.is('li')) {
+            element = element.parent().addClass('in').parent();
+        } else {
+            break;
+        }
+    }
+});
